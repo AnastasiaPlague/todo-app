3d4bc2f1b1dbf435005f6cee1bf9a52b8d957a5a
Anastasia <great-swindler@mail.ru>
2019-12-03 22:16:45 +0300

    Add new item to the list

--- src/components/ItemAddForm/ItemAddForm.js
+++ src/components/ItemAddForm/ItemAddForm.js
@@ -1,20 +1,38 @@
-import React, { Component } from 'react';
-import './ItemAddForm.css';
+import React, { Component } from "react";
+import "./ItemAddForm.css";
 
 class ItemAddForm extends Component {
- 
-  render() { 
-    return (
-			<div className="item-add-form">
-				<input type="text" className="form-control item-add-form" />
-				<button
-					className="btn btn-outline-secondary float-right"
-					onClick={() => this.props.onAdded("Hello")}>
-					Add Item
-				</button>
-			</div>
+	state = {
+		label: ""
+	};
+
+	onLabelChange = e => {
+		this.setState({
+			label: e.target.value
+		});
+	};
+	onSubmit = e => {
+		e.preventDefault();
+		this.props.onAdded(this.state.label);
+		this.setState({
+			label: ""
+		});
+	};
+
+	render() {
+		return (
+			<form className="item-add-form d-flex" onSubmit={this.onSubmit}>
+				<input
+					type="text"
+					className="form-control"
+					placeholder="input your todo"
+					value={this.state.label}
+					onChange={this.onLabelChange}
+				/>
+				<button className="btn btn-outline-secondary">Add</button>
+			</form>
 		);
-  }
+	}
 }
- 
+
 export default ItemAddForm;
